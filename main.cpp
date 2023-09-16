e0b6ffaa7303f09d1c9dffe0a47b35b6fa48f51c
Emma <144365111+emmabilh@users.noreply.github.com>
2023-09-15 22:11:54 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Emma Bilhimer
+// C++ 2023
+// Due: 09/15/2023
+//Lab 1
+//Description: This lab will serve as a demonstration of your ability to download a project from GitHub, compile and run the project, upload a project to GitHub, and submit a project to Canvas. This lab assumes you have downloaded and installed an IDE (integrated development environment) and have also installed GitHub Desktop. If you have not met the assumptions for the lab, the steps you need to complete the lab may vary from what is here. The goals are for you to download a project, compile and run that project, and to upload your own project. If you meet the goals, you will get the points for this lab. Follow the directions in this lab and complete the tasks.
+
+//  main.cpp
+//  MyFirstProject
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! " << endl;
+    system("Color 9");
+    return 0;
+}
